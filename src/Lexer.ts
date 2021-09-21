b0dee1ec169a7ee8f44f95cb9bd8361ba7509b86
Tan Kian <thejianmail@gmail.com>
2021-09-21 23:24:45 +0800

    Implement readChar method for Lexer

--- src/Lexer.ts
+++ src/Lexer.ts
@@ -1,11 +1,34 @@
 import type { Token } from './Token'
 
-export { scan };
+export { Lexer };
 
-const scan = (str: string): Array<Token> => {
-  const len = str.length;
-  let i = 0;
-  while (i < len) {
+class Lexer {
+  private cur: number = 0;
 
+  constructor(
+    private input: string,
+  ) {}
+
+  /**
+   * Return the next character of the input string or undefined if we have reach the end
+   */
+  readChar(): string|undefined {
+    if (this.cur >= this.input.length) return;
+    const nextChar = this.input[this.cur];
+    this.cur += 1;
+    return nextChar;
+  }
+
+  /**
+   * Return the next token or undefined if we have reach the end
+   */
+  getToken(): Token|undefined {
+    let c = this.readChar(); // the next non-whitespace character
+    while(c && /\s/.test(c)) c = this.readChar();
+    if (!c) return;
+
+    switch (c) {
+
+    }
   }
 }
